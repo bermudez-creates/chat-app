5449a7ac2cb377a75015c9c03079c77912accdcb
Steezy <bermudezalejandro45@gmail.com>
2024-02-16 12:45:17 -0500

    Login controller logic added - Checking user creds.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,19 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import generateTokenAndSaveCookie from '../utils/generateToken.js';
 
 export const signup = async (req, res) => {
   try {
+    //inputs
     const { fullName, username, password, confirmPassword, gender } = req.body;
-
+    //check pass
     if (password !== confirmPassword) {
       return res.status(400).json({ error: 'Passwords do not match.' });
     }
-
+    // check if user exist
     const user = await User.findOne({ username });
-
+    // if exist return error
+    // if not create one
     if (user) {
       return res.json(401).json({ error: 'Username already exists.' });
     }
@@ -31,24 +34,55 @@ export const signup = async (req, res) => {
       profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
     });
 
-    //saves new user to DB
-    await newUser.save();
+    // checks if user is new
+    if (newUser) {
+      //saves new user to DB
+      generateTokenAndSaveCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: 'Invalid user data.' });
+    }
   } catch (error) {
     console.log(`Error in sign up controller`, error);
     res.status(501).json({ error: 'Internal server error.' });
   }
 };
 
-export const login = (req, res) => {
-  console.log(`User`);
-  res.send('User');
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcryptjs.compare(
+      password,
+      user?.password || ''
+    );
+
+    if (!user || !isPasswordCorrect) {
+      res.status(402).json({ error: 'Invalid username or password' });
+    }
+
+    if (user && isPasswordCorrect) {
+      generateTokenAndSaveCookie(user._id, res);
+
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic,
+      });
+    }
+  } catch (error) {
+    console.log(generateTokenAndSaveCookie);
+    console.log(`Error in login controller`, error.message);
+    res.status(502).json({ error: 'Internal server error' });
+  }
 };
 
 export const logout = (req, res) => {
