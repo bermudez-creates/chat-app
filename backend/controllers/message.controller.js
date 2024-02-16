d53d333988a11d1752f4754f9f87040d3a65820e
Steezy <bermudezalejandro45@gmail.com>
2024-02-16 14:22:33 -0500

    Send message endpoint added

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,5 @@
+// import Message from '../models/message.model';
+
+export const sendMessage = async (req, res) => {
+  console.log('Message sent !');
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -11,6 +12,7 @@ dotenv.config();
 app.use(express.json()); // Parse JSON payloads request coming from (req.body)
 //middleware used to communicate with database using endpoint
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 // app.get('/', (req, res) => {
 //   res.send('Hello');
