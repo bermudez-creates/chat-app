9f5202313591b58fe8435b4ac1ff099ab3907d21
Steezy <bermudezalejandro45@gmail.com>
2024-02-24 17:56:07 -0500

    Router Dom added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css';
 import Login from './pages/login/Login.jsx';
 import SignUp from './pages/signup/SignUp.jsx';
 import Home from './pages/home/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      {/* <SignUp /> */}
-      {/* <Login /> */}
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
