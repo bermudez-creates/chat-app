d7de4befa58b3a4d9c17b68bd3a43bab3a58cbac
Steezy <bermudezalejandro45@gmail.com>
2024-02-28 15:43:59 -0500

    Signup/Logout functionality added - Context folder added

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { GrLogout } from 'react-icons/gr';
+import useLogout from '../../hooks/useLogout';
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
+
   return (
     <div className="mt-auto">
-      <GrLogout className="w-5 h-6 cursor-pointer text-purple-800" />
+      <GrLogout
+        className="w-5 h-6 cursor-pointer text-purple-800"
+        onClick={logout}
+      />
     </div>
   );
 };
